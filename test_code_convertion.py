a08cbd81aad4098f4b69588cfa0afe2b66c46fa0
wulin <52038994@qq.com>
2019-05-09 02:29:13 +0800

    Save data directly into a excel file, so that it is easy to navigate the data.

--- test_code_convertion.py
+++ test_code_convertion.py
@@ -9,6 +9,7 @@ from src.data.dictionary import Dictionary
 import io
 import numpy as np
 import sys
+import xlwt
 
 
 def printCodesInChars(codes, dict):
@@ -18,12 +19,27 @@ def printCodesInChars(codes, dict):
             print(dict[codes[iRow][iCol]], end=' ')
         print()
 
+def saveCodesInCharsInExcel(codes, dict, ofilename):
+    writebook = xlwt.Workbook()
+    sheets = []
+    sheet = writebook.add_sheet('data')
+    sheets.append(sheet)
+    nRows , nCols = codes.shape
+    for iRow in range(nRows):
+        for iCol in range(nCols):
+            iData = int( iCol / 256 )
+            if (len(sheets) <= iData) :
+                sheets.append(writebook.add_sheet('data' + str(iData)))
+            iColInExcel = iCol % 256
+            sheets[iData].write(iRow, iColInExcel, dict[codes[iRow][iCol]])
+    writebook.save(ofilename)
 
 def mytest20190508():
     dict = Dictionary.read_vocab('data/processed/en-zh/vocab.en-zh')
     dataToConvert = np.loadtxt(sys.argv[1], np.int32)
 
     printCodesInChars(dataToConvert, dict)
+    saveCodesInCharsInExcel(dataToConvert, dict, sys.argv[1] + ".xls")
 
 # https://www.cnblogs.com/feng18/p/5646925.html
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
