e87357b6d1f18efbb86f1bfd4a0a6ddc2b8fdaff
liukecai <liukecai8@hotmail.com>
2020-04-27 16:57:45 +0800

    Add translation system main page.

--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 import platform
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 if platform.system() == 'windows':
     app = Flask(import_name=__name__,
@@ -17,6 +17,19 @@ app.debug = True
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    content = {
+        "text": request.form["text"],
+        "lang1": request.form["lang1"],
+        "lang2": request.form["lang2"]
+    }
+
+    result = content['text']
+    return jsonify({"result": result}), 200
+
 
 if __name__ == "__main__":
     app.run()
